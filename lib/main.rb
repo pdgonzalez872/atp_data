5e148d9decaa1ea4111aaea5c895d40fe2aa000f
pdgonzalez872 <pdgonzalez872@gmail.com>
2016-10-09 01:23:18 -0500

    Add ranking

--- lib/main.rb
+++ lib/main.rb
@@ -50,14 +50,18 @@ module Atp
     player_data['first_name'] = doc.css(".player-profile-hero-name .first-name").text
     player_data['last_name'] = doc.css(".player-profile-hero-name .last-name").text
     player_data['country'] = doc.css(".player-flag-code").text
+    player_data['ranking'] = raw_ranking(ranking: doc.css(".data-number").text)
     player_data['birthday'] = raw_birthday(messy_birthday: doc.css(".table-birthday").text)
     player_data['prize_money'] = doc.css('#playersStatsTable tr').last.css('td').last.children[1].attributes['data-doubles'].value.gsub('$', '')
-    binding.pry
     puts player_data
   end
 
   def self.raw_birthday(messy_birthday:)
     messy_birthday.gsub(/\t/, '').gsub(/\r\n/, '').gsub('(', '').gsub(')', '').gsub('.', '-')
+  end
+
+  def self.raw_ranking(ranking:)
+    ranking.gsub(/\t/, '').gsub(/\r\n/, '')
   end
 end
 
