8350437bd048ecaf654d290507262afb2d7ad903
pdgonzalez872 <pdgonzalez872@gmail.com>
2016-10-09 08:41:16 -0500

    Add metrics to script

--- lib/main.rb
+++ lib/main.rb
@@ -12,8 +12,10 @@ player_data_csv = "#{Dir.pwd}/data/player_data_20161003.csv"
 
 puts "Fetching rankings"
 #urls = ATPDataGatherer.fetch_data_for(page: local_page) # remote_page trigger is here
+time_before_ranking_request = Time.now
 urls = ATPDataGatherer.fetch_data_for(page: remote_page(date:'2016-10-03')) # remote_page trigger is here
-puts "Fetching complete"
+time_after_ranking_request = Time.now
+puts "Fetching complete, took #{time_after_ranking_request - time_before_ranking_request} seconds"
 
 
 puts "Creating file"
@@ -24,6 +26,9 @@ puts "File created"
 
 puts "Iterating through urls and parsing each single player page"
 urls.each do |url|
+
+  time_before_each_url = Time.now
+
   data = ATPDataGatherer.parse_player_page(player_page: open(url))
   File.open(player_data_csv, "a") do |f|
     f.puts("#{data['ranking']}," \
@@ -39,6 +44,8 @@ urls.each do |url|
          "#{data['birthday']}," \
          "#{data['prize_money']}")
   end
+  time_after_each_url = Time.now
+  puts "Took #{time_after_each_url - time_before_each_url} seconds"
 end
 puts "Iteration complete"
 
