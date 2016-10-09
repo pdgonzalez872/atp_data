c4843eb9b97307e43822ec835931a296477cc813
pdgonzalez872 <pdgonzalez872@gmail.com>
2016-10-08 23:06:07 -0500

    have the list of urls, will start looping over them soon

--- lib/main.rb
+++ lib/main.rb
@@ -1,5 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 # page = File.open("rankings.htm")
 #:puts page
@@ -8,5 +9,20 @@ dummy_path = "#{Dir.pwd}/lib/rankings.html"
 
 page = File.open(dummy_path, "r")
 
-stuff = Nokogiri::HTML(page)
-puts stuff
+doc = Nokogiri::HTML(page)
+
+# This gives the href for Djoko
+# doc.css('td.player-cell').first.children[1]['href']
+
+# this is the root url
+root_url = "http://www.atpworldtour.com"
+
+urls = []
+
+doc.css('td.player-cell').each do |row|
+  urls << row.children[1]['href']
+end
+
+urls.each do |url|
+  puts "#{root_url}#{url}"
+end
